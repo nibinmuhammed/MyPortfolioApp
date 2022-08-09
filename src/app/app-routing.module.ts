1179fd219317b4a928050f90608a51b221635011
nibinmuhammed <102959918+nibinmuhammed@users.noreply.github.com>
2022-08-09 15:04:05 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+
+const routes: Routes = [
+  {
+    path: 'experience',
+    loadChildren: () => import('./components/experience/experience.module').then(m => m.ExperienceModule)
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./components/contact/contact.module').then(m => m.ContactModule)
+  },
+  {
+    path: 'projects',
+    loadChildren: () => import('./components/projects/projects.module').then(m => m.ProjectsModule)
+  },
+  {
+    path: 'skills',
+    loadChildren: () => import('./components/skills/skills.module').then(m => m.SkillsModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
